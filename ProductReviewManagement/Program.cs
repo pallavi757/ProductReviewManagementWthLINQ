f28dc47ee43647a0c0e661ed871f2555cb3d142a
unknown <kinekarpallavi21@gmail.com>
2022-04-30 22:28:44 +0530

    [Pallavi]Add.Created List of review and inserted values to it

---
+++ ProductReviewManagement/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+namespace ProductReviewManagement
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Product Review Management Program.");
+            Console.WriteLine("-------------------------------------------------------------");
+            //UC1
+            // add elements of the custom classes using the collection-initializer syntax.
+            // The following adds objects of the ProductReview class in the List<ProductReview>.
+            List<ProductReview> productReviewList = new List<ProductReview>()
+            {
+                //Add Custom Class Objects in List
+                new ProductReview(){ProductID=1,UserID=1,Rating=2,Review="Good",isLike=true},
+                new ProductReview(){ProductID=2,UserID=1,Rating=4,Review="Good",isLike=true},
+                new ProductReview(){ProductID=3,UserID=1,Rating=5,Review="Good",isLike=true},
+                new ProductReview(){ProductID=4,UserID=1,Rating=6,Review="Good",isLike=false},
+                new ProductReview(){ProductID=5,UserID=1,Rating=2,Review="nice",isLike=true},
+                new ProductReview(){ProductID=6,UserID=1,Rating=1,Review="bas",isLike=true},
+                new ProductReview(){ProductID=8,UserID=1,Rating=1,Review="Good",isLike=false},
+                new ProductReview(){ProductID=8,UserID=1,Rating=9,Review="nice",isLike=true},
+                new ProductReview(){ProductID=2,UserID=1,Rating=10,Review="nice",isLike=true},
+                new ProductReview(){ProductID=10,UserID=1,Rating=8,Review="nice",isLike=true},
+                new ProductReview(){ProductID=11,UserID=1,Rating=3,Review="nice",isLike=true},
+                new ProductReview(){ProductID=12,UserID=1,Rating=2,Review="Good",isLike=true},
+                new ProductReview(){ProductID=12,UserID=1,Rating=4,Review="Good",isLike=true},
+                new ProductReview(){ProductID=13,UserID=1,Rating=5,Review="Good",isLike=true},
+                new ProductReview(){ProductID=14,UserID=1,Rating=6,Review="Good",isLike=false},
+                new ProductReview(){ProductID=15,UserID=1,Rating=2,Review="nice",isLike=true},
+                new ProductReview(){ProductID=15,UserID=1,Rating=1,Review="bad",isLike=true},
+                new ProductReview(){ProductID=15,UserID=1,Rating=2,Review="bad",isLike=true},
+                new ProductReview(){ProductID=15,UserID=1,Rating=1,Review="Good",isLike=false},
+                new ProductReview(){ProductID=18,UserID=1,Rating=9,Review="nice",isLike=true},
+                new ProductReview(){ProductID=19,UserID=1,Rating=10,Review="nice",isLike=true},
+                new ProductReview(){ProductID=20,UserID=1,Rating=8,Review="nice",isLike=true},
+                new ProductReview(){ProductID=21,UserID=1,Rating=3,Review="nice",isLike=true},
+                new ProductReview(){ProductID=22,UserID=1,Rating=10,Review="nice",isLike=true},
+                new ProductReview(){ProductID=22,UserID=1,Rating=8,Review="nice",isLike=true},
+                new ProductReview(){ProductID=25,UserID=1,Rating=3,Review="nice",isLike=true}
+
+            };
+            // using foreach LINQ method
+            // Query execution
+            foreach (var list in productReviewList)
+            {
+                Console.WriteLine("ProductID:" + list.ProductID + " " + "UserID: " + list.UserID
+                    + " " + "Rating: " + list.Rating + " " + "Review: " + list.Review + " " + "isLike: " + list.isLike);
+                Console.WriteLine("-------------------------------------------------------------");
+            }
+        }
+    }
+}
